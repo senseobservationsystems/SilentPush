23a80d9406bc75bbdcd947e381012ef59ff7f701
Ole Begemann <ole@oleb.net>
2016-03-11 12:34:35 +0100

    Register for push notifications

--- SilentPush/SilentPush/AppDelegate.swift
+++ SilentPush/SilentPush/AppDelegate.swift
@@ -6,7 +6,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Badge], categories: nil)
+        application.registerUserNotificationSettings(notificationSettings)
+        application.registerForRemoteNotifications()
         return true
+    }
+
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        print("\(__FUNCTION__): \(notificationSettings)")
+    }
+
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("\(__FUNCTION__): \(deviceToken)")
+    }
+
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("\(__FUNCTION__): \(error)")
+    }
+
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        print("\(__FUNCTION__): \(notification)")
+    }
+
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        print("\(__FUNCTION__): \(userInfo)")
+        print("Calling completion handler")
+        completionHandler(.NewData)
     }
 
 }
